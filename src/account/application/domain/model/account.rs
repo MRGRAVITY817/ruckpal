1037998f622893771e6110a9b68f8d5a81470b20
Hoon Wee <mrgravity817@gmail.com>
2023-10-29 01:47:35 +0900

    feat: add deposit

--- src/account/application/domain/model/account.rs
+++ src/account/application/domain/model/account.rs
@@ -16,21 +16,29 @@ impl Account {
         }
     }
 
+    /// Calculate the balance of current account.
     pub fn calculate_balance(&self) -> Money {
         self.base_line_balance + self.activity_window.calculate_balance(self.id)
     }
 
+    /// Withdraw money from current account to target account.
     pub fn withdraw(self, money: Money, target_account_id: AccountId) -> Self {
-        match self.may_withdraw(money) {
-            false => self,
-            true => {
-                let withdrawal =
-                    Activity::new(self.id, self.id, target_account_id, Local::now(), money);
-                let activity_window = self.activity_window.add_activity(withdrawal);
-
-                Self::new(self.id, self.base_line_balance, activity_window)
-            }
+        if self.may_withdraw(money) {
+            return self;
         }
+
+        let withdrawal = Activity::new(self.id, self.id, target_account_id, Local::now(), money);
+        let activity_window = self.activity_window.add_activity(withdrawal);
+
+        Self::new(self.id, self.base_line_balance, activity_window)
+    }
+
+    /// Deposit money from source account to current account.
+    pub fn deposit(self, money: Money, source_account_id: AccountId) -> Self {
+        let deposit = Activity::new(self.id, source_account_id, self.id, Local::now(), money);
+        let activity_window = self.activity_window.add_activity(deposit);
+
+        Self::new(self.id, self.base_line_balance, activity_window)
     }
 
     /// Check if account can withdraw the given amount of money.
