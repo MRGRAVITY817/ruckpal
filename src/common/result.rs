0d635657f713dd1dfcf712144f7050fcc905feef
Hoon Wee <mrgravity817@gmail.com>
2023-11-04 22:56:03 +0900

    feat: add thiserror

--- src/common/result.rs
+++ src/common/result.rs
@@ -1,3 +1,9 @@
-use std::error::Error;
+use thiserror::Error as ThisError;
 
-pub type AppResult<T> = Result<T, Box<dyn Error>>;
+pub type AppResult<T> = Result<T, AppError>;
+
+#[derive(ThisError, Debug)]
+pub enum AppError {
+    #[error("database disconnected")]
+    DatabaseDisconnected,
+}
